502a9a47a3e7b30b6ad5a6034d2bcf2bba533d62
FreshWaterLee <ihg9303@gmail.com>
2021-08-17 16:44:37 +0900

    08171645

--- MediaPipe_Holistic_Project/Collet_Keypoint.py
+++ MediaPipe_Holistic_Project/Collet_Keypoint.py
@@ -4,45 +4,57 @@ import os
 import cv2
 import time
 
-mp_holistic = mp.solutions.holistic # Holistic model
-mp_drawing = mp.solutions.drawing_utils # MediaPipe 라이브러리를 통해 검출된 포즈, 손 등을 시각화 시켜주는 변수
+mp_holistic = mp.solutions.holistic  # Holistic model
+mp_drawing = mp.solutions.drawing_utils  # MediaPipe 라이브러리를 통해 검출된 포즈, 손 등을 시각화 시켜주는 변수
 
-def mediapipe_detection(image,model): # 입력 모델에 따라 객체 출력 함수(ex: 손 검출 모델을 넣으면 손이 검출,포즈면 포즈, )
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # 모델링에 입력하기위해 이미지의 픽셀 타입 변경
+
+def mediapipe_detection(image, model):  # 입력 모델에 따라 객체 출력 함수(ex: 손 검출 모델을 넣으면 손이 검출,포즈면 포즈, )
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # 모델링에 입력하기위해 이미지의 픽셀 타입 변경
     image.flags.writeable = False
-    results = model.process(image) # 입력 모델에 현 프레임을 입력하여 모델에서 검출되는 결과를 변수화
+    results = model.process(image)  # 입력 모델에 현 프레임을 입력하여 모델에서 검출되는 결과를 변수화
     image.flags.writeable = True
-    image =cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # 전처리 이전의 이미지로 복구
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # 전처리 이전의 이미지로 복구
     return image, results
     # 현 프레임과 검출 결과값 리턴
 
-def draw_landmarks(image,results): # 검출된 랜드마크(모델에 따라 검출된 포인트(손, 얼굴, 신체등))를 시각화 해주는 함수
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS) # 얼굴 랜드마크 시각화
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # 포즈 랜드마크 시각화(신체)
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # 왼손으로 검출된 포인트 시각화
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # 오른손으로 검출된 포인트 시각화
 
-def draw_styled_landmarks(image,results):
+def draw_landmarks(image, results):  # 검출된 랜드마크(모델에 따라 검출된 포인트(손, 얼굴, 신체등))를 시각화 해주는 함수
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)  # 얼굴 랜드마크 시각화
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)  # 포즈 랜드마크 시각화(신체)
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)  # 왼손으로 검출된 포인트 시각화
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)  # 오른손으로 검출된 포인트 시각화
+
+
+def draw_styled_landmarks(image, results):
     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS,
-                              mp_drawing.DrawingSpec(color=(80,110,10),thickness=1,circle_radius=1),
+                              mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
                               mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
                               )
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, mp_drawing.DrawingSpec(color=(80,22,10),thickness=1,circle_radius=1),
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=1, circle_radius=1),
                               mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=1, circle_radius=1)
                               )
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, mp_drawing.DrawingSpec(color=(121,22,76),thickness=1,circle_radius=1),
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=1, circle_radius=1),
                               mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=1, circle_radius=1)
                               )
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, mp_drawing.DrawingSpec(color=(245,117,66),thickness=1,circle_radius=1),
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=1, circle_radius=1),
                               mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=1, circle_radius=1)
                               )
 
+
 def extract_keypoint(results):
-    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
-    face = np.array([[res.x, res.y, res.z, res.visibility] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(1404)
-    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21 * 3)
-    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21 * 3)
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() \
+        if results.pose_landmarks else np.zeros(33 * 4)
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() \
+        if results.face_landmarks else np.zeros(468 * 3)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() \
+        if results.left_hand_landmarks else np.zeros(21 * 3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() \
+        if results.right_hand_landmarks else np.zeros(21 * 3)
     return np.concatenate([pose, face, lh, rh])
+
 
 DATA_PATH = os.path.join('MP_Data')
 actions = np.array(['hello', 'thanks', 'iloveyou'])
@@ -50,7 +62,7 @@ sequence_length = 30
 no_sequences = 30
 cap = cv2.VideoCapture(0)
 
-with mp_holistic.Holistic(min_detection_confidence=0.5,min_tracking_confidence=0.5) as holistic:
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     for action in actions:
         for sequence in range(sequence_length):
             for frame_num in range(sequence_length):
@@ -62,8 +74,10 @@ with mp_holistic.Holistic(min_detection_confidence=0.5,min_tracking_confidence=0
                 draw_styled_landmarks(image, results)
 
                 if frame_num == 0:
-                    cv2.putText(image, 'STARTING COLLECTION', (120, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4, cv2.LINE_AA)
-                    cv2.putText(image, 'Collecting Frame for {} Video Number {}'.format(action, sequence), (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    cv2.putText(image, 'STARTING COLLECTION', (120, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4,
+                                cv2.LINE_AA)
+                    cv2.putText(image, 'Collecting Frame for {} Video Number {}'.format(action, sequence), (15, 12),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
                     cv2.waitKey(2000)
                 else:
                     cv2.putText(image, 'Collecting Frame for {} Video Number {}'.format(action, sequence), (15, 12),
--- MediaPipe_Holistic_Project/Train_data.py
+++ MediaPipe_Holistic_Project/Train_data.py
@@ -1,14 +1,67 @@
 import mediapipe as mp
 import numpy as np
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.utils import to_categorical
 import os
-import time
+import warnings
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import multilabel_confusion_matrix,accuracy_score
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM,Dense
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.models import load_model
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
 
 DATA_PATH = os.path.join('MP_Data')
 actions = np.array(['hello', 'thanks', 'iloveyou'])
 sequence_length = 30
+no_sequences = 30
 
+
+## 라벨링 및 랜드마크(검출 포인트) 불러오기
 label_map ={label:num for num, label in enumerate(actions)}
+sequences, labels = [], []
+for action in actions:
+    for sequence in range(no_sequences):
+        window = []
+        for frame_num in range(sequence_length):
+            res = np.load(os.path.join(DATA_PATH, action, str(sequence), "{}.npy".format(frame_num)))
+            window.append(res)
+        sequences.append(window)
+        labels.append(label_map[action])
 
-print(label_map)
+X = np.array(sequences)
+
+Y = to_categorical(labels).astype(int)
+## 테스트, 트레인 데이터 분류
+x_train,x_test,y_train,y_test = train_test_split(X, Y, test_size= 0.05)
+
+### 트레이닝 시작
+log_dir =os.path.join('Logs')
+tb_callback = TensorBoard(log_dir=log_dir)
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
+#model.summary()
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+model.fit(x_train, y_train, epochs=2000, callbacks=[tb_callback])
+
+res = model.predict(x_test)
+actions[np.argmax(res[4])]
+
+actions[np.argmax(y_test[4])]
+
+model.save('Action_0817.h5')
+del model
+model = load_model('Action_0817.h5')
+yhat =model.predict(x_test)
+ytrue = np.argmax(y_test,axis=1).tolist()
+yhat = np.argmax(yhat,axis=1).tolist()
+multilabel_confusion_matrix(ytrue,yhat)
+print(accuracy_score(ytrue,yhat))
