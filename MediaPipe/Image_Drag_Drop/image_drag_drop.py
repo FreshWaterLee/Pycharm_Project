39ce9e24bf144f5615094d09d8dd56d1833fd26d
FreshWaterLee <ihg9303@gmail.com>
2021-09-10 16:12:16 +0900

    09101630

--- MediaPipe/Image_Drag_Drop/image_drag_drop.py
+++ MediaPipe/Image_Drag_Drop/image_drag_drop.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28231cf73132fa04fe8c6b08d5faa7fabd5080c52f4fe8ef3f70267b9f766b55
-size 780
+oid sha256:0aa68cafb8c19a4db70f20db5e1b6c1a2d893fa463b0bb5c248110448835ff9d
+size 2157
