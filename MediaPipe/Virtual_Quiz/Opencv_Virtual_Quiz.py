f02d94dd684f470dbca0d670a8ef0b2f46607f47
FreshWaterLee <ihg9303@gmail.com>
2021-09-23 16:41:53 +0900

    0923

--- MediaPipe/Virtual_Quiz/Opencv_Virtual_Quiz.py
+++ MediaPipe/Virtual_Quiz/Opencv_Virtual_Quiz.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db660e11e7c0ccd40ccc05e3ce22b6f7e3747af658f971f61cf1df05b768c734
-size 759
+oid sha256:e9a83289f7a6fae365b7052eab001910061680d89cd77d0d01bcd039ae18de08
+size 765
---
+++ Yolo/GPU_check.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:47c37901f040496a05f2a41ee1368c8d4f99ecd2848502bc9a9ab9079080b80d
+size 89
