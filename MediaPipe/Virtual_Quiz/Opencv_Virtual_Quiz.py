3633fb9e5a386f6da235e551b1212599d4b47246
FreshWaterLee <ihg9303@gmail.com>
2021-09-24 16:25:11 +0900

    1630

--- MediaPipe/Virtual_Quiz/Opencv_Virtual_Quiz.py
+++ MediaPipe/Virtual_Quiz/Opencv_Virtual_Quiz.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9a83289f7a6fae365b7052eab001910061680d89cd77d0d01bcd039ae18de08
-size 765
+oid sha256:79caa4bda8d5aca4901aa5f99afcf460511f2009728cf7152b51cbef295c8ef3
+size 1994
